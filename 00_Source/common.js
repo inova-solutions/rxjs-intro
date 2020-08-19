d0418c1ab7fbb072e6d89100c2a1c0cbaf54f1be
Markus Chiarot <markusc@inova.ch>
2020-08-19 14:06:40 +0200

    improve log

--- 00_Source/common.js
+++ 00_Source/common.js
@@ -1,6 +1,16 @@
 const chalk = require('chalk');
 
-const log = console.log;
+const originalConsoleLog = console.log;
+const log = (...args) => originalConsoleLog(time(), ...args);
+
+const time = () => {
+    const d = new Date();
+    const h = d.getHours().toString().padStart(2, '0');
+    const m = d.getMinutes().toString().padStart(2, '0');
+    const s = d.getSeconds().toString().padStart(2, '0');
+    const ms = d.getMilliseconds();
+    return chalk.grey(`[${h}:${m}:${s}.${ms}]`);
+}
 
 console.success = (...args) => log(chalk.green(...args));
 console.error = (...args) => log(chalk.red('Error:', ...args));
