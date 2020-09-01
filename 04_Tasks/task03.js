cc906785cf1fca9f481e0cb55fab6d38f1d3d168
Markus Chiarot <markusc@inova.ch>
2020-09-01 18:13:42 +0200

    change task 3

--- 04_Tasks/task03.js
+++ 04_Tasks/task03.js
@@ -1,25 +1,25 @@
 require('../00_Source/common');
-const { of } = require("rxjs");
+const { of, BehaviorSubject, combineLatest } = require('rxjs');
+const { delay, withLatestFrom, mergeMap, concatMap } = require('rxjs/operators');
 
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
+const availableDrink = new BehaviorSubject('Water');
+setTimeout(() => availableDrink.next('Sprite'), 1200);
+setTimeout(() => availableDrink.next('Fanta'), 2200);
+
+function getPerson() {
+    return of(
+        'Max Muster',
+        'Peter Silie',
+        'Hans Heinzelmann',
+        'Kurt Kontrabass',
+        'Arnold Anders'
+    ).pipe(concatMap(person => of(person).pipe(delay(500))));
 }
 
-function fruitsAPI() {
-    return of(...shuffleArray(['Apple', 'Banana', 'Pear', 'Orange', 'Mango', 'Pinapple']));
-}
-
-function weekdaysAPI() {
-    return of('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
-}
-
-
-// for every weekday associate the first fruit from the API
-// build a short phrase for every weekday in this form:
-// Menu for Monday: Apple
-// Menu for Tuesday: Mango
+// For every person get the currently available drink
+// and form a sentence like:
+// Max Muster drinks a cup of Water
+// Hans Heinzelmann drinks a cup of Sprite
 // ...
+getPerson().pipe(withLatestFrom(availableDrink.asObservable()))
+    .subscribe(([person, drink]) => console.log(`${person} drinks a cup of ${drink}`));
